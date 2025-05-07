e56e28b9ec36305ff58158e4286a329ac28a8491
yo <32672663+yoyoyo0205@users.noreply.github.com>
2025-05-07 21:49:59 +0900

    サンプルリクエストをSwaggerに表示

--- src/main/java/com/example/hello_springboot/controller/HelloController.java
+++ src/main/java/com/example/hello_springboot/controller/HelloController.java
@@ -6,12 +6,15 @@ import com.example.hello_springboot.dto.HelloResponse;
 import com.example.hello_springboot.service.HelloService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.ExampleObject;
 import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.parameters.RequestBody;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import jakarta.validation.Valid;
+
 import java.time.LocalDateTime;
 
 @RestController
@@ -23,9 +26,24 @@ public class HelloController {
             summary = "あいさつメッセージを返すAPI",
             description = "nameを指定してPOSTすると、その名前を含んだあいさつを返します。"
     )
+    @RequestBody(
+            required = true,
+            content = @Content(
+                    mediaType = "application/json",
+                    examples = {
+                            @ExampleObject(
+                                    name = "サンプルリクエスト",
+                                    summary = "通常のリクエスト例",
+                                    description = "Airiさんに挨拶を送る例",
+                                    value = "{\"name\": \"Airi\"}"
+                            )
+                    }
+            )
+
+    )
     @ApiResponses(value = {
             @ApiResponse(
-                    responseCode="200",
+                    responseCode = "200",
                     description = "正常にメッセージが返されました。",
                     content = @Content(
                             mediaType = "application/json",
@@ -47,10 +65,10 @@ public class HelloController {
     }
 
     @PostMapping("/hello")
-    public HelloResponse postHello(@Valid @RequestBody HelloRequest request) {
+    public HelloResponse postHello(@Valid @org.springframework.web.bind.annotation.RequestBody HelloRequest request) {
         String message = "Hello, " + request.getName() + "!";
         LocalDateTime now = LocalDateTime.now();
-        return new HelloResponse(message,now);
+        return new HelloResponse(message, now);
 
     }
 }
