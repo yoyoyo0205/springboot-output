8891af848de76084d8712b8f1ed4f3b53852fae2
yo <32672663+yoyoyo0205@users.noreply.github.com>
2025-05-08 08:48:01 +0900

    @OpenAPIDefinition 追加

--- src/main/java/com/example/hello_springboot/HelloSpringbootApplication.java
+++ src/main/java/com/example/hello_springboot/HelloSpringbootApplication.java
@@ -1,7 +1,17 @@
 package com.example.hello_springboot;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@OpenAPIDefinition(
+		info = @Info(
+				title = "あいさつAPI",
+				version = "1.0.0",
+				description = "これは学習用のSpring Boot + Swagger のAPIドキュメントです。"
+		)
+)
 
 @SpringBootApplication
 public class HelloSpringbootApplication {
