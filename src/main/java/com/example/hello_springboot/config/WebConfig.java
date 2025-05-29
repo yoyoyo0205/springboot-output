b65892a015a679a754e964e6fddf0c26f3db0ce8
yo <32672663+yoyoyo0205@users.noreply.github.com>
2025-05-29 21:33:29 +0900

    CORS設定許可（Reactアプリの受け入れ）

--- src/main/java/com/example/hello_springboot/config/WebConfig.java
+++ src/main/java/com/example/hello_springboot/config/WebConfig.java
@@ -1,6 +1,7 @@
 package com.example.hello_springboot.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -11,4 +12,14 @@ public class WebConfig implements WebMvcConfigurer {
     public void addInterceptors(InterceptorRegistry registry){
         registry.addInterceptor(new LoggingInterceptor());
     }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry){
+        registry.addMapping("/**") //全てのパスに対して
+                .allowedOrigins("http://localhost:3000") //ReactのURL
+                .allowedMethods("GET","POST","PUT","DELETE","OPTIONS")
+                .allowedHeaders("*")
+                .allowCredentials(true); //必要ならCookieやAuthorozationヘッダーを許可
+    }
 }
+
